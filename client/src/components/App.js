cff05fe40086167536463b7bf2b8a659ad2ea221
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-12-19 11:47:22 +0530

    setting up react-router and wiring up routes to all the stream components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
-const PageOne = () => {
-  return <div>PageOne</div>;
-};
+import StreamCreate from "./Streams/StreamCreate";
+import StreamDelete from "./Streams/StreamDelete";
+import StreamEdit from "./Streams/StreamEdit";
+import StreamList from "./Streams/StreamList";
+import StreamShow from "./Streams/StreamShow";
 
-const PageTwo = () => {
-  return <div>PageTwo</div>;
-};
+import Header from './Header';
 
 const App = () => {
   return (
     <div>
+    <Header />
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pageTwo"  component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
