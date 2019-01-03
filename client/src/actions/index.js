349d410e2ea2f866ead14fa4d54e72a22dad6a10
Tejas Bhosale <bhosaletejas93@gmail.com>
2019-01-03 15:14:05 +0530

    --Deleting Streams working-- -used react portals to create a modal message popup to ask about whether you want to delete stream or not. -connected the deleteStream action generator to delte stream.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -54,6 +54,7 @@ export const deleteStream = id => async dispatch => {
     type: DELETE_STREAM,
     payload: id
   });
+  history.push("/");
 };
 
 export const editStream = (id, formValues) => async dispatch => {
